b66e6433c074f0cc961607b1409ef61607a704ea
Sameer Ahmed <sameer.ssj786@gmail.com>
2022-02-25 06:04:46 +0500

    almost done

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,23 +8,25 @@ const Header = () => {
     getCategories().then((result) => setCategories(result));
   }, []);
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-0">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              SH BLOG
-            </span>
-          </Link>
-        </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((data) => (
-            <Link key={data.slug} href={`/category/${data.slug}`}>
-              <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer text-white">
-                {data.name}
+    <div className="bg-black">
+      <div className="container mx-auto py-8 px-10 mb-8">
+        <div className="w-full inline-block">
+          <div className="md:float-left block">
+            <Link href="/">
+              <span className="cursor-pointer font-bold text-4xl transition duration-500 hover:text-pink-500 transform text-white">
+                SH BLOG
               </span>
             </Link>
-          ))}
+          </div>
+          <div className="hidden md:float-left md:contents">
+            {categories.map((data, index) => (
+              <Link key={index} href={`/category/${data.slug}`}>
+                <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer transition duration-500 hover:text-pink-500 text-white">
+                  {data.name}
+                </span>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,7 @@
 import React from "react";
 import { getPosts, getPostDetails } from "../../Services";
+import { useRouter } from "next/router";
+import { AdjacentPosts } from "../../section";
 import {
   PostDetail,
   Categories,
@@ -7,14 +9,21 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from "../../Components";
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
           <Author author={post.author} />
+          <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
           <CommentsForm slug={post.slug} />
           <Comments slug={post.slug} />
         </div>
